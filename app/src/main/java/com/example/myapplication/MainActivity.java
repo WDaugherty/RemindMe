187dc0dbf157f80bef57a8f8f2d5a499070dcf1a
Ryan Hurlock <hurlora19@juniata.edu>
2021-04-15 22:57:44 -0400

    fixed wacky errors

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,15 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-<<<<<<< HEAD
+
 import com.google.android.libraries.places.api.net.PlacesClient;
 
 public class MainActivity extends AppCompatActivity {
     PlacesClient placesClient;
-=======
-public class MainActivity extends AppCompatActivity {
 
->>>>>>> bf6f4794179236a71d4cb1b449662d6c453938cd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
