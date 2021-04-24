439e68e1c22845623ef75a797a012945c981a8c9
WDaugherty <78816087+WDaugherty@users.noreply.github.com>
2021-04-24 14:37:48 -0400

    Update CreateFragment.java

--- app/src/main/java/com/example/myapplication/ui/task/CreateFragment.java
+++ app/src/main/java/com/example/myapplication/ui/task/CreateFragment.java
@@ -8,6 +8,7 @@
 * Contract File:
 *   * Verify Table Columns are With Correct Entry (Some were mixed up, I made changes)
 *   * Decide on TEXT or INTEGER for DateTime column. I suggest INTEGER. Methods are psuedo-coded for each below
+    UPDATE: Verified table columns and decieded on text for datetime, this is subject to change.
 *
 * DB Helper:
 *   * Verify that all needed SQL statements exist.
